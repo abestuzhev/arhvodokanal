15fdea11465c9d74571a6805d69c3962894a9c2d
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-10-16 00:37:38 +0300

    Initial commit

---
+++ online-pay.js
@@ -0,0 +1,102 @@
+$(function(){
+
+$('.personal-account__form').each(function(){
+  $(':required').on('blur', function() {
+    $(this).parents('.personal-account__item').addClass('error');
+    if($(this).val() != '') {
+        $(this).parents('.personal-account__item').removeClass('error');
+      }
+  })
+});
+
+$('#personal-account').on('focus', function(){
+    $('.personal-reference > div').removeClass('personal-reference__active');
+    $('.personal-reference__account').addClass('personal-reference__active');
+});
+
+$('#personal-phone').on('focus', function(){
+  $('.personal-reference > div').removeClass('personal-reference__active');
+  $('.personal-reference__phone').addClass('personal-reference__active');
+});
+
+$('#personal-email').on('focus', function(){
+  $('.personal-reference > div').removeClass('personal-reference__active');
+  $('.personal-reference__email').addClass('personal-reference__active');
+});
+
+$('#personal-account, #personal-phone, #personal-email').on('blur', function(){
+    $('.personal-reference > div').removeClass('personal-reference__active');
+});
+
+/*mask*/
+
+$('#personal-account').mask('0000000');
+$('#personal-phone').mask('+7(000)000-00-00');
+
+});
+
+
+$(function() {
+
+  $('.personal-accoun__form').each(function(){
+    // Объявляем переменные (форма и кнопка отправки)
+	var form = $(this),
+        btn = form.find('.personal-account__btn');
+
+    // Добавляем каждому проверяемому полю, указание что поле пустое
+	form.find('.personal-account__input').addClass('empty_field');
+
+    // Функция проверки полей формы
+    function checkInput(){
+      form.find('.personal-account__input').each(function(){
+        if($(this).val() != ''){
+          // Если поле не пустое удаляем класс-указание
+		          $(this).removeClass('empty_field');
+        } else {
+          // Если поле пустое добавляем класс-указание
+		          $(this).addClass('empty_field');
+        }
+      });
+    }
+
+    // Функция подсветки незаполненных полей
+    function lightEmpty(){
+      form.find('.empty_field').css({'border-color':'#d8512d'});
+      // Через полсекунды удаляем подсветку
+      setTimeout(function(){
+        form.find('.empty_field').removeAttr('style');
+      },500);
+    }
+
+    // Проверка в режиме реального времени
+    setInterval(function(){
+      // Запускаем функцию проверки полей на заполненность
+	  checkInput();
+      // Считаем к-во незаполненных полей
+      var sizeEmpty = form.find('.empty_field').lenght;
+      // console.log(sizeEmpty);
+      // Вешаем условие-тригер на кнопку отправки формы
+      if(sizeEmpty > 0){
+        if(btn.hasClass('disabled')){
+          return false
+        } else {
+          btn.addClass('disabled')
+        }
+      } else {
+        btn.removeClass('disabled')
+      }
+    },500);
+
+    // Событие клика по кнопке отправить
+    btn.click(function(){
+      if($(this).hasClass('disabled')){
+        // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
+		      lightEmpty();
+        return false
+      } else {
+        // Все хорошо, все заполнено, отправляем форму
+        form.submit();
+      }
+    });
+  });
+});
